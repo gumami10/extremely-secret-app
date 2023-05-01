e0e77e43324e106e91ad4d63c3e298f02ec83454
Eric Groppe <emmgp@hotmail.com>
2023-05-01 15:36:48 -0300

    Updates

---
+++ precache-manifest.f0606486f615589fe2c22937a9c34e57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd2ed839391befd059e442c18261bd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d3aad7a80938871dd4c",
+    "url": "/static/css/main.43d0d931.chunk.css"
+  },
+  {
+    "revision": "ac1504c1af657a88dfc9",
+    "url": "/static/js/2.2b58d58e.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.2b58d58e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3aad7a80938871dd4c",
+    "url": "/static/js/main.7b9ad6a6.chunk.js"
+  },
+  {
+    "revision": "3127fa9c9b5520db3630",
+    "url": "/static/js/runtime-main.b5e2c1b7.js"
+  },
+  {
+    "revision": "f354587496302817e18f6f0c3a7fc1c2",
+    "url": "/static/media/canvas.f3545874.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/extremely-secret-app/precache-manifest.a289dc312fbd7be641a80d9809998218.js"
+  "/precache-manifest.f0606486f615589fe2c22937a9c34e57.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/extremely-secret-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
